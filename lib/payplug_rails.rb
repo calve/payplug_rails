035f018649182c5b276ecb939b6a8500666040c7
calve <calvinh34@gmail.com>
2015-04-09 16:37:01 +0200

    Now create payments URLs

--- lib/payplug_rails.rb
+++ lib/payplug_rails.rb
@@ -1,12 +1,31 @@
 require "payplug_rails/version"
+require 'openssl'
+require 'base64'
+require 'json'
+require 'cgi'
 
 module PayplugRails
-  # Your code goes here...
-  mattr_accessor :baseurl
+
+  mattr_accessor :url
   mattr_accessor :private_key
   mattr_accessor :payplug_public_key
 
-  def self.hello
-    puts "hello, payment"
+  class Payplug
+
+    def self.create_payment(amount, ipn_url, customer_hash={})
+      data = {
+        currency: 'EUR',
+        amount: amount,
+        ipn_url: ipn_url,
+
+      }.merge(customer_hash).to_query
+      key = OpenSSL::PKey::RSA.new PayplugRails.private_key
+      raw_sign = key.sign(OpenSSL::Digest::SHA1.new, data)
+      http_sign = CGI.escape(Base64.encode64(raw_sign))
+      http_data = CGI.escape(Base64.encode64(data))
+      puts data
+      return PayplugRails.url+"?data="+http_data+"&sign="+http_sign
+    end
+
   end
 end
